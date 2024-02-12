297bb5158bc985f1e466ec0ab0e18279614afc72
Ritchie Paul Buitre <ritchie@richorelse.com>
2024-02-12 20:31:42 +0800

    Add GoBucks::UserDecorator

---
+++ app/decorators/go_bucks/user_decorator.rb
@@ -0,0 +1,14 @@
+module GoBucks
+  module UserDecorator
+    extend ActiveSupport::Concern
+
+    included do
+      has_one :wallet, class_name: "GoBucks::Wallet"
+    end
+    
+    def wallet() = super || build_wallet
+    def wallet_balance() = wallet.balance
+  end
+end
+
+User.include UserDecorator
--- app/decorators/user_decorator.rb
+++
@@ -1,6 +0,0 @@
-User.class_eval do
-  has_one :wallet, class_name: "GoBucks::Wallet"
-
-  def wallet() = super || build_wallet
-  def wallet_balance() = wallet.balance
-end
